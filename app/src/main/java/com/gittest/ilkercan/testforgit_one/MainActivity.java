7987b20533002fee96f74ccccb279f2935600dfd
ilker can <ilker@pineal.com.tr>
2016-12-23 17:38:55 +0200

    C

--- app/src/main/java/com/gittest/ilkercan/testforgit_one/MainActivity.java
+++ app/src/main/java/com/gittest/ilkercan/testforgit_one/MainActivity.java
@@ -2,12 +2,28 @@ package com.gittest.ilkercan.testforgit_one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        InitButton();
+    }
+
+    private void InitButton()
+    {
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
     }
 }
